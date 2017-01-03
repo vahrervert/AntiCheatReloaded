db135274989f2e79385e970ac819ca556653f815
Rammelkast <mail@rammelkast.nl>
2017-01-03 16:14:29 +0100

    Fix #26

---
+++ src/main/java/net/gravitydevelopment/anticheat/util/NMS_1_9_PLUS.java
@@ -0,0 +1,15 @@
+package net.gravitydevelopment.anticheat.util;
+
+import java.util.EnumSet;
+
+import org.bukkit.Material;
+
+public class NMS_1_9_PLUS {
+
+	static final EnumSet<Material> MOVE_UP_BLOCKS_1_9 = EnumSet.of(Material.ACACIA_STAIRS,
+			Material.BIRCH_WOOD_STAIRS, Material.BRICK_STAIRS, Material.COBBLESTONE_STAIRS, Material.DARK_OAK_STAIRS,
+			Material.JUNGLE_WOOD_STAIRS, Material.NETHER_BRICK_STAIRS, Material.QUARTZ_STAIRS,
+			Material.RED_SANDSTONE_STAIRS, Material.SANDSTONE_STAIRS, Material.SMOOTH_STAIRS,
+			Material.SPRUCE_WOOD_STAIRS, Material.WOOD_STAIRS, Material.PURPUR_STAIRS);
+	
+}
--- src/main/java/net/gravitydevelopment/anticheat/util/VersionUtil.java
+++ src/main/java/net/gravitydevelopment/anticheat/util/VersionUtil.java
@@ -19,12 +19,6 @@ public class VersionUtil {
 			Material.RED_SANDSTONE_STAIRS, Material.SANDSTONE_STAIRS, Material.SMOOTH_STAIRS,
 			Material.SPRUCE_WOOD_STAIRS, Material.WOOD_STAIRS);
 	
-	private static final EnumSet<Material> MOVE_UP_BLOCKS_1_9 = EnumSet.of(Material.ACACIA_STAIRS,
-			Material.BIRCH_WOOD_STAIRS, Material.BRICK_STAIRS, Material.COBBLESTONE_STAIRS, Material.DARK_OAK_STAIRS,
-			Material.JUNGLE_WOOD_STAIRS, Material.NETHER_BRICK_STAIRS, Material.QUARTZ_STAIRS,
-			Material.RED_SANDSTONE_STAIRS, Material.SANDSTONE_STAIRS, Material.SMOOTH_STAIRS,
-			Material.SPRUCE_WOOD_STAIRS, Material.WOOD_STAIRS, Material.PURPUR_STAIRS);
-	
 	public static boolean isFlying(Player p) {
 		if (getVersion().equals("v1_9_R1") || getVersion().equals("v1_10_R1") || getVersion().equals("v1_11_R1")) {
 			return p.isFlying() || p.isGliding() || p.hasPotionEffect(PotionEffectType.LEVITATION);
@@ -35,7 +29,7 @@ public class VersionUtil {
 
 	public static EnumSet<Material> getMoveUpBlocks() {
 		if (getVersion().equals("v1_9_R1") || getVersion().equals("v1_10_R1") || getVersion().equals("v1_11_R1")) {
-			return MOVE_UP_BLOCKS_1_9;
+			return NMS_1_9_PLUS.MOVE_UP_BLOCKS_1_9;
 		} else {
 			return MOVE_UP_BLOCKS_1_8;
 		}
